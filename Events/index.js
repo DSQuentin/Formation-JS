43525fc17cc8a1910a0771a3a36605162233ce87
Quentin <dsquentin@hotmail.fr>
2021-03-18 16:24:05 +0100

    add data course

--- Events/index.js
+++ Events/index.js
@@ -2,8 +2,8 @@ const btn = document.querySelector('#btn'); // selectionne le btn qui a l'id btn
 const img = document.getElementById('img'); // autre facon de selectionner
 
 btn.addEventListener('click', () => {
-    img.classList.toggle('show'); // toggle vérifie si on l'a, et fait l'inverse
-})
+  img.classList.toggle('show'); // toggle vérifie si on l'a, et fait l'inverse
+});
 
 /** --------------------------------------------------------- */
 
@@ -12,22 +12,22 @@ const horizontal = document.querySelector('.horizontal');
 const vertical = document.querySelector('.vertical');
 
 mouseEvent.addEventListener('mousemove', (event) => { // event => objet js contenant information de l'event, ici mousemove
-    horizontal.innerHTML = event.x; // innerHTML => change la valeur de l'html
-    vertical.innerHTML = event.y;
-    mouseEvent.style.left = event.x / window.innerWidth * 100 + "%"; // style pour changer le css d'un id
+  horizontal.innerHTML = event.x; // innerHTML => change la valeur de l'html
+  vertical.innerHTML = event.y;
+  mouseEvent.style.left = `${(event.x / window.innerWidth) * 100}%`; // style pour changer le css d'un id
 
-    if (event.x > 1000) {
-        document.body.style.filter = "blur(3px)";
-    } else {
-        document.body.style.filter = "none";
-    }
-})
+  if (event.x > 1000) {
+    document.body.style.filter = 'blur(3px)';
+  } else {
+    document.body.style.filter = 'none';
+  }
+});
 
 /** --------------------------------------------------------- */
 
 document.getElementById('input').addEventListener('input', (e) => {
-    vertical.innerHTML = e.target.value;
-})
+  vertical.innerHTML = e.target.value;
+});
 
 /** --------------------------------------------------------- */
 
@@ -35,21 +35,21 @@ const theme = document.querySelectorAll('.theme');
 
 theme.forEach((item) => {
   item.addEventListener('click', (e) => {
-    console.log(e.target.id)
-    document.body.classList.remove('darkTheme', "yellowTheme", "salmonTheme");
+    console.log(e.target.id);
+    document.body.classList.remove('darkTheme', 'yellowTheme', 'salmonTheme');
 
-    switch(e.target.id){
-      case "dark":
+    switch (e.target.id) {
+      case 'dark':
         document.body.classList.add('darkTheme');
         break;
-      case "salmon":
+      case 'salmon':
         document.body.classList.add('salmonTheme');
         break;
-      case "yellow":
+      case 'yellow':
         document.body.classList.add('yellowTheme');
         break;
       default:
         null;
     }
-  })
-})
+  });
+});
