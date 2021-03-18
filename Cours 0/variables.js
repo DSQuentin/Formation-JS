e2ce14460fc52f2c7eefda01d725f9af87533908
Quentin <dsquentin@hotmail.fr>
2021-03-18 16:53:04 +0100

    reorganisation

---
+++ Cours 0/variables.js
@@ -0,0 +1,12 @@
+// variables
+// let -> evolue
+let cours = "oivhefovhe"
+console.log(`${cours} // avant maj`)
+cours = 'eofivhoefh'
+console.log(`${cours} // après maj`)
+
+
+// const -> inchangeable
+const prenom = 'patrice'
+console.log(prenom)
+prenom = 24 // Impossible
